866887b92d059d93583adbeac391ebaeb1dee5b6
Yosh <gonzolezu@gmail.com>
2023-08-29 19:44:12 -0700

    create DrawingView

--- app/src/main/java/com/jigcoding/doodlekong/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/jigcoding/doodlekong/ui/drawing/DrawingActivity.kt
@@ -1,10 +1,14 @@
 package com.jigcoding.doodlekong.ui.drawing
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.jigcoding.doodlekong.R
 import com.jigcoding.doodlekong.databinding.ActivityDrawingBinding
+import com.jigcoding.doodlekong.util.Constants.DEFAULT_PAINT_THICKNESS
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
@@ -26,10 +30,32 @@ class DrawingActivity : AppCompatActivity() {
         }
     }
 
+    private fun selectColor(color: Int) {
+        binding.drawingView.setColor(color)
+        binding.drawingView.setThickness(DEFAULT_PAINT_THICKNESS)
+    }
+
     private fun subscribeToUiStateUpdates() {
         lifecycleScope.launchWhenStarted {
             viewModel.selectedColorButtonId.collect { id ->
                 binding.colorGroup.check(id)
+                when (id) {
+                    R.id.rbBlack -> selectColor(Color.BLACK)
+                    R.id.rbBlue -> selectColor(Color.BLUE)
+                    R.id.rbGreen -> selectColor(Color.GREEN)
+                    R.id.rbOrange -> selectColor(
+                        ContextCompat.getColor(
+                            this@DrawingActivity,
+                            R.color.orange
+                        )
+                    )
+                    R.id.rbYellow -> selectColor(Color.YELLOW)
+                    R.id.rbRed -> selectColor(Color.RED)
+                    R.id.rbEraser -> {
+                        binding.drawingView.setColor(Color.WHITE)
+                        binding.drawingView.setThickness(40f)
+                    }
+                }
             }
         }
     }
